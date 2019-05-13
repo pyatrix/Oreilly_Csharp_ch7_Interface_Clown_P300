9da2c7138b65c91efd9c942c5dab7a3b5fe6e1fa
Peng <Peng@PENG-PC>
2019-05-13 10:54:22 +0800

    加入專案檔案。

---
+++ Oreilly_Csharp_ch7_Interface_Clown_P300/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_ch7_Interface_Clown_P300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
