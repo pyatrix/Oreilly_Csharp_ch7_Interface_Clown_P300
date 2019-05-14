8e598227574cf66c413c6f9e155292457596eb52
Peng <Peng@PENG-PC>
2019-05-14 10:44:41 +0800

    p314 finish

--- Oreilly_Csharp_ch7_Interface_Clown_P300/Program.cs
+++ Oreilly_Csharp_ch7_Interface_Clown_P300/Program.cs
@@ -10,9 +10,17 @@ namespace Oreilly_Csharp_ch7_Interface_Clown_P300
     {
         static void Main(string[] args)
         {
+            ScaryScary fingersTheClown = new ScaryScary("big shoes", 14);
+            FunnyFunny someFunnyClown = fingersTheClown;
+            IScaryClown someOtherScaryClown = someFunnyClown as ScaryScary;
+            someOtherScaryClown.Honk();
+
+            
             TallGuy tallGuy = new TallGuy() { Height = 74, Name = "Jimmy" };
             tallGuy.TalkAboutYourself();
             tallGuy.Honk();
+            
+
             Console.ReadKey();
         }
     }
